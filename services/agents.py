4eecb49cd03f3dd8d6b28ba9ad647c7631673c1d
Karl Lorey <git@karllorey.com>
2025-01-15 14:53:05 +0100

    Fix syntax error

--- services/agents.py
+++ services/agents.py
@@ -5,5 +5,5 @@ from domain.models import Action, Signal
 
 
 class Agent(ABC):
-    def process_signals(self, signals: list[Signal]) -> list[Action]
+    def process_signals(self, signals: list[Signal]) -> list[Action]:
         raise NotImplemented()
